db585a647bc1018f225703ea9f06159cf1411341
yuchiXiong <yuchi.xiong@foxmail.com>
2024-12-16 19:03:55 +0800

    draft: 暂时移除 getSession 方法

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
-import { getServerSession } from "next-auth/next"
-import { Session } from "next-auth"
+// import { getServerSession } from "next-auth/next"
+// import { Session } from "next-auth"
 import { authOptions } from "@/utils/auth-config"
 
 declare module "next-auth" {
@@ -10,19 +10,19 @@ declare module "next-auth" {
   }
 }
 
-export async function getSession(): Promise<Session | null> {
-  const session = await getServerSession(authOptions) as Session | null
-  if (!session) {
-    console.log('No session found')
-    return null
-  }
-  if (!session.accessToken) {
-    console.log('Session found, but no access token')
-  } else {
-    console.log('Session found with access token')
-  }
-  return session
-}
+// export async function getSession(): Promise<Session | null> {
+//   const session = await getServerSession(authOptions) as Session | null
+//   if (!session) {
+//     console.log('No session found')
+//     return null
+//   }
+//   if (!session.accessToken) {
+//     console.log('Session found, but no access token')
+//   } else {
+//     console.log('Session found with access token')
+//   }
+//   return session
+// }
 
 
 const handler = NextAuth(authOptions)
